0b5acaa59a516ec3286ff2b60f81bad41c5230f5
Johnny Hsiao <johnnyhsiao87@gmail.com>
2015-06-25 20:07:04 -0400

    Initial Commit

---
+++ Epic Movies/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  Epic Movies
+//
+//  Created by Shark on 2015-06-25.
+//  Copyright (c) 2015 cs2680. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
